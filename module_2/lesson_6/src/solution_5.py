105184ffa5ae4c9c0c7dafc84787cf1b762691f0
Volt Husky <mrdjstim@gmail.com>
2024-04-23 20:40:33 +0300

    lesson_6 completed again

--- module_2/lesson_6/src/solution_5.py
+++ module_2/lesson_6/src/solution_5.py
@@ -1,5 +1,7 @@
 prices = input("Введите цены: ")
 new_prices = [int(sort_prices) for sort_prices in prices.split(',')]
 new_prices.sort()
-new_prices[0], new_prices[3] = new_prices[3], new_prices[0]
+new_prices_max = new_prices.index(max(new_prices))
+new_prices_min = new_prices.index(min(new_prices))
+new_prices[new_prices_max], new_prices[new_prices_min] = new_prices[new_prices_min], new_prices[new_prices_max]
 print(f"Новые цены: {new_prices}")
